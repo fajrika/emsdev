fa46d08c01f3c2d2c7402e72a7dceabeefd752f0
dandoridwanto <dandoridwanto@ymail.com>
2020-08-19 09:59:17 +0700

    Update P_kirim_konfirmasi_tagihan.php

--- application/controllers/Transaksi/P_kirim_konfirmasi_tagihan.php
+++ application/controllers/Transaksi/P_kirim_konfirmasi_tagihan.php
@@ -697,8 +697,8 @@ class P_kirim_konfirmasi_tagihan  extends CI_Controller
         require_once 'vendor/MPDF/vendor/autoload.php';
         $mpdf = new \Mpdf\Mpdf(['mode'=>'utf-8', 'format'=>'A4']);
         // $mpdf = new \Mpdf\Mpdf(['mode'=>'utf-8', 'format'=>'A4', 'orientation' => 'L']);
-        ini_set('max_execution_time', '300'); 
-        ini_set("pcre.backtrack_limit", -1);
+        ini_set('max_execution_time', '300');
+        ini_set("pcre.backtrack_limit", "9000000");
         ob_start();
         ?>
         <!DOCTYPE html>
